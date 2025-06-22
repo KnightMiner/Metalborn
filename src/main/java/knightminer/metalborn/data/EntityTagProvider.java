bca94d67da0cb627e5e321a1b824ba52c707ca30
KnightMiner <knightminer4@gmail.com>
2025-06-21 22:26:10 -0400

    Add some tags for future hemalurgy
    just the core 8 effects for now

---
+++ src/main/java/knightminer/metalborn/data/EntityTagProvider.java
@@ -0,0 +1,38 @@
+package knightminer.metalborn.data;
+
+import knightminer.metalborn.Metalborn;
+import knightminer.metalborn.metal.MetalId;
+import net.minecraft.core.HolderLookup.Provider;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.EntityTypeTagsProvider;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+/** Adds tags for entities */
+public class EntityTagProvider extends EntityTypeTagsProvider {
+  public EntityTagProvider(PackOutput packOutput, CompletableFuture<Provider> lookupProvider, @Nullable ExistingFileHelper existingFileHelper) {
+    super(packOutput, lookupProvider, Metalborn.MOD_ID, existingFileHelper);
+  }
+
+  @Override
+  protected void addTags(Provider pProvider) {
+    tag(MetalIds.iron).add(EntityType.GHAST, EntityType.PHANTOM, EntityType.HOGLIN, EntityType.ZOGLIN, EntityType.RAVAGER);
+    tag(MetalIds.steel).add(EntityType.SPIDER, EntityType.CAVE_SPIDER);
+    tag(MetalIds.tin).add(EntityType.SKELETON, EntityType.STRAY);
+    tag(MetalIds.pewter).add(EntityType.CREEPER);
+    tag(MetalIds.copper).add(EntityType.PILLAGER, EntityType.VINDICATOR, EntityType.ILLUSIONER, EntityType.WITCH);
+    tag(MetalIds.bronze).add(EntityType.ZOMBIE, EntityType.ZOMBIE_VILLAGER, EntityType.HUSK);
+    tag(MetalIds.gold).add(EntityType.PIGLIN, EntityType.PIGLIN_BRUTE, EntityType.ZOMBIFIED_PIGLIN);
+    tag(MetalIds.roseGold).add(EntityType.SLIME); // TODO: tinkers slime
+  }
+
+  /** Creates a tag for a metal */
+  public IntrinsicTagAppender<EntityType<?>> tag(MetalId metal) {
+    return tag(TagKey.create(Registries.ENTITY_TYPE, metal));
+  }
+}
